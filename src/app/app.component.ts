df1c869817da15779ad315e2cbed4127aa837268
Andrey Babenko <andrey.babenko@dataart.com>
2023-05-08 01:49:10 +0300

    selectors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {select, Store} from "@ngrx/store";
+import {select, State, Store} from "@ngrx/store";
 import {Observable} from "rxjs";
 import {map} from 'rxjs/operators';
 import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
+import { AppState } from './reducers';
+import { isLoggedIn, isLoggedOut } from './auth/auth.selectors';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +14,21 @@ import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Route
 export class AppComponent implements OnInit {
 
     loading = true;
+    isLoggedIn$: Observable<boolean>;
+    isLoggedOut$: Observable<boolean>;
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private store: Store<AppState>) {
 
     }
 
     ngOnInit() {
+      this.isLoggedIn$ = this.store.pipe(
+        select(isLoggedIn)
+      );
+      this.isLoggedOut$ = this.store.pipe(
+        select(isLoggedOut)
+      );
+
 
       this.router.events.subscribe(event  => {
         switch (true) {
---
+++ src/app/auth/action-types.ts
@@ -0,0 +1,3 @@
+import * as AuthActions from './auth.acions';
+
+export {AuthActions};
